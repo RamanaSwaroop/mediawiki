89cd8aa3716941ff24cf343e8d6db4e2008e417f
Ramana Swaroop Chandrashekar <rachandr@microsoft.com>
2022-02-23 01:28:30 +0530

    undo old changes

--- provider.tf
+++ provider.tf
@@ -14,8 +14,8 @@ terraform {
 }
 
 provider "azurerm" {
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
+  #tenant_id       = var.tenant_id
+  #subscription_id = var.subscription_id
   features {}
 }
 
