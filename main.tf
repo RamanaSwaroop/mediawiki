30c9ed1d9ae49ba92399ffeb3a831d3074493f2a
Ramana Swaroop Chandrashekar <rachandr@microsoft.com>
2022-03-02 11:05:46 +0530

    add modules

--- main.tf
+++ main.tf
@@ -1,7 +1,44 @@
 module "rg" {
-  source   = "./modules/rg"
+  source                  = "./modules/rg"
   resource_group_name     = var.resource_group_name
   resource_group_location = var.resource_group_location
-  resource_tags     = var.resource_tags
+  resource_tags           = var.resource_tags
 }
 
+module "network" {
+  source = "./modules/network"
+  depends_on = [
+    module.rg
+  ]
+  vnet_name          = var.vnet_name
+  vnet_address_space = var.vnet_address_space
+  subnets            = var.subnets
+  nsg                = var.nsg
+  lb_name            = var.lb_name
+}
+
+module "vm" {
+  source = "./modules/vm"
+  depends_on = [
+    module.rg,
+    module.network
+  ]
+  vm_name     = var.vm_name
+  vm_size     = var.vm_size
+  vm_image_id = var.vm_image_id
+  vm_username = var.vm_username
+  script_path = var.script_path
+  image_rg    = var.image_rg
+  vm_subnet   = var.vm_subnet
+}
+
+module "app-gw" {
+  source = "./modules/appgw"
+  depends_on = [
+    module.rg,
+    module.network,
+    module.vm
+  ]
+  agw_name   = var.agw_name
+  agw_subnet = var.agw_subnet
+}
--- variables.tf
+++ variables.tf
@@ -9,3 +9,75 @@ variable "resource_group_location" {
 variable "resource_tags" {
   type = map(string)
 }
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+}
+
+variable "subnets" {
+  type = list(object({
+    name             = string
+    address_prefixes = list(string)
+  }))
+}
+
+variable "nsg" {
+  type = list(object({
+    name = string
+    security_rules = list(object({
+      name                       = string
+      protocol                   = string
+      direction                  = string
+      access                     = string
+      priority                   = number
+      source_port_range          = string
+      source_address_prefix      = string
+      destination_port_range     = string
+      destination_address_prefix = string
+    }))
+  }))
+}
+
+variable "lb_name" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "vm_size" {
+  type = string
+}
+
+variable "vm_image_id" {
+  type = string
+}
+
+variable "vm_username" {
+  type = string
+}
+
+variable "script_path" {
+  type = string
+}
+
+variable "image_rg" {
+  type = string
+}
+
+variable "vm_subnet" {
+  type = string
+}
+
+variable "agw_name" {
+  type = string
+}
+
+variable "agw_subnet" {
+  type = string
+}
